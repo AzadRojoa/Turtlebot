0937bee92f82015e5a5f77ff924946cba8c24840
Your AMadelpech <aimy.madelpech@ynov.com>
2024-11-19 14:42:43 +0100

    navigation auto_maj

--- autonomous_nav2/autonomous_nav2/autonomous_nav2.py
+++ autonomous_nav2/autonomous_nav2/autonomous_nav2.py
@@ -4,101 +4,86 @@ import rclpy
 from rclpy.duration import Duration
 
 """
-Basic navigation demo to go to pose.
+Basic navigation demo to go to pose and return to initial position.
 """
-
 
 def main():
     rclpy.init()
 
     navigator = BasicNavigator()
 
-    # Set our demo's initial pose
+    # Set the robot's initial pose
     initial_pose = PoseStamped()
     initial_pose.header.frame_id = 'map'
     initial_pose.header.stamp = navigator.get_clock().now().to_msg()
-    initial_pose.pose.position.x = 0.0
-    initial_pose.pose.position.y = 0.0
+    initial_pose.pose.position.x = 0.03
+    initial_pose.pose.position.y = 0.01
     initial_pose.pose.orientation.z = 0.0
     initial_pose.pose.orientation.w = 1.0
     navigator.setInitialPose(initial_pose)
 
-    # Activate navigation, if not autostarted. This should be called after setInitialPose()
-    # or this will initialize at the origin of the map and update the costmap with bogus readings.
-    # If autostart, you should `waitUntilNav2Active()` instead.
-    # navigator.lifecycleStartup()
-
-    # Wait for navigation to fully activate, since autostarting nav2
+    # Activate navigation
     navigator.waitUntilNav2Active()
 
-    # If desired, you can change or load the map as well
-    # navigator.changeMap('/path/to/map.yaml')
+    # First goal pose
+    goal_pose1 = PoseStamped()
+    goal_pose1.header.frame_id = 'map'
+    goal_pose1.header.stamp = navigator.get_clock().now().to_msg()
+    goal_pose1.pose.position.x = 3.81
+    goal_pose1.pose.position.y = 1.1
+    goal_pose1.pose.orientation.w = 1.0
+    goal_pose1.pose.orientation.z = 0.0
 
-    # You may use the navigator to clear or obtain costmaps
-    # navigator.clearAllCostmaps()  # also have clearLocalCostmap() and clearGlobalCostmap()
-    # global_costmap = navigator.getGlobalCostmap()
-    # local_costmap = navigator.getLocalCostmap()
 
-    # Go to our demos first goal pose
-    goal_pose = PoseStamped()
-    goal_pose.header.frame_id = 'map'
-    goal_pose.header.stamp = navigator.get_clock().now().to_msg()
-    goal_pose.pose.position.x = 17.86
-    goal_pose.pose.position.y = -0.77
-    goal_pose.pose.orientation.w = 1.0
-    goal_pose.pose.orientation.z = 0.0
+    # Navigate to the first goal
+    navigator.goToPose(goal_pose1)
 
-    # sanity check a valid path exists
-    # path = navigator.getPath(initial_pose, goal_pose)
-
-    navigator.goToPose(goal_pose)
-
-    i = 0
     while not navigator.isTaskComplete():
-        ################################################
-        #
-        # Implement some code here for your application!
-        #
-        ################################################
-
-        # Do something with the feedback
-        i = i + 1
         feedback = navigator.getFeedback()
-        if feedback and i % 5 == 0:
+        if feedback:
             print(
-                'Estimated time of arrival: '
-                + '{0:.0f}'.format(
-                    Duration.from_msg(feedback.estimated_time_remaining).nanoseconds
-                    / 1e9
-                )
-                + ' seconds.'
+                f"Estimated time of arrival: "
+                f"{Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9:.0f} seconds."
             )
 
-            # Some navigation timeout to demo cancellation
-            if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
-                navigator.cancelTask()
-
-            # Some navigation request change to demo preemption
-            if Duration.from_msg(feedback.navigation_time) > Duration(seconds=18.0):
-                goal_pose.pose.position.x = 0.0
-                goal_pose.pose.position.y = 0.0
-                navigator.goToPose(goal_pose)
-
-    # Do something depending on the return code
+    # Check the result of the first navigation task
     result = navigator.getResult()
     if result == TaskResult.SUCCEEDED:
-        print('Goal succeeded!')
+        print('First goal succeeded! Returning to initial position...')
     elif result == TaskResult.CANCELED:
-        print('Goal was canceled!')
+        print('First goal was canceled! Returning to initial position anyway...')
     elif result == TaskResult.FAILED:
-        print('Goal failed!')
+        print('First goal failed! Returning to initial position anyway...')
     else:
-        print('Goal has an invalid return status!')
+        print('First goal has an invalid return status! Returning to initial position anyway...')
 
+    # Return to the initial position
+    navigator.goToPose(initial_pose)
+
+    while not navigator.isTaskComplete():
+        feedback = navigator.getFeedback()
+        if feedback:
+            print(
+                f"Returning to initial position. "
+                f"Estimated time of arrival: "
+                f"{Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9:.0f} seconds."
+            )
+
+    # Final result for returning to the initial position
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print('Successfully returned to initial position!')
+    elif result == TaskResult.CANCELED:
+        print('Return to initial position was canceled!')
+    elif result == TaskResult.FAILED:
+        print('Failed to return to initial position!')
+    else:
+        print('Return to initial position has an invalid return status!')
+
+    # Shutdown the navigator
     navigator.lifecycleShutdown()
 
     exit(0)
-
 
 if __name__ == '__main__':
     main()
