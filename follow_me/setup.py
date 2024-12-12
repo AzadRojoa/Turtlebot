9c2280b1847664bc85ded0dcad20227c57721934
LegliseV <valougujan33470@gmail.com>
2024-12-12 18:38:40 +0100

    Modif du setup.py pour autoriser le lancement via le launch file

--- follow_me/setup.py
+++ follow_me/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'follow_me'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +23,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'follow_me = follow_me.follow_me:main',
             'follow = follow_me.test_follow:main'
         ],
     },
